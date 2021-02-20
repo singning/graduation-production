4fbbae3495962181e8cc4800678c4afdac2bcac7
zhangkejing <1030092508@qq.com>
2021-02-20 11:29:43 +0800

    1

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import styles from './index.css';
+
+export default function () {
+  return (
+    <div className={styles.normal}>
+      <div className={styles.welcome} />
+      <ul className={styles.list}>
+        <li>
+          111111111111111111
+        </li>
+      </ul>
+    </div>
+  );
+}
